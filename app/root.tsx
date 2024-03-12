6a408d5d8538790d2563ee5461286b16c72d6024
maluyan <maluyan@cn-software.net>
2024-03-12 16:48:24 +0800

    1

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,29 @@ import { ConfigProvider } from 'antd';
 import zhCN from 'antd/locale/zh_CN';
 import 'dayjs/locale/zh-cn';
 
-export function Layout({ children }: { children: React.ReactNode }) {
+// export function Layout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <html lang="en">
+//       <head>
+//         <meta charSet="utf-8" />
+//         <meta name="viewport" content="width=device-width, initial-scale=1" />
+//         <Meta />
+//         <Links />
+//       </head>
+//       <body>
+//         <ConfigProvider
+//           locale={zhCN}
+//         >
+//           {children}
+//         </ConfigProvider>
+//         <ScrollRestoration />
+//         <Scripts />
+//       </body>
+//     </html>
+//   );
+// }
+
+export default function App() {
   return (
     <html lang="en">
       <head>
@@ -22,15 +44,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <ConfigProvider
           locale={zhCN}
         >
-          {children}
+          <Outlet />
         </ConfigProvider>
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
   );
-}
-
-export default function App() {
-  return <Outlet />;
 }
